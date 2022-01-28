889cf6c03dfb7e4428701a7fe46bad95e81f265c
Egor <gladin.el@phystech.edu>
2022-01-28 13:47:51 +0100

    Put experiment with full sampling into a separate file. Improved plotting. Introduced other minor changes.

--- experiment_barycenter.py
+++ experiment_barycenter.py
@@ -4,16 +4,14 @@ Experiment: find barycenter for a simple 3x3 setup.
 import numpy as np
 import torch
 from torch.distributions.multivariate_normal import MultivariateNormal
+from torch.distributions import constraints
 import ot
 import scipy.stats
+import time
+import gc
 
 from algorithm import algorithm
 from utils import safe_log, plot_trajectory, norm_sq
-
-
-IMG_SIZE = 3  # image size is 3x3
-n = IMG_SIZE**2  # barycenter dimensionality
-FLOAT64 = torch.float64  # avoid errors when converting from torch to numpy
 
 
 def get_cost_matrix(im_sz, device, dtype=torch.float32):
@@ -35,61 +33,16 @@ def get_cost_matrix(im_sz, device, dtype=torch.float32):
     return C / (im_sz - 1)**2
 
 
-def marginals_residuals(r, Xs, cs):
-    """
-    Compute squared norms of residuals for marginal distributions.
-
-    :param r: (n,) tensor (barycenter)
-    :param Xs: (m, n, n) tensor (transportation plans)
-    :param cs: (m, n) tensor (distributions, data points)
-    :return: list of 2 scalar tensors
-    """
-    margin_r = Xs.sum(dim=-1) - r  # (m, n) tensor
-    margin_c = Xs.sum(dim=1) - cs  # (m, n) tensor
-    return norm_sq(margin_r), norm_sq(margin_c)
-
-
-def barycenter_objective(r, Xs, cost_mat, kappas, cs):
-    """
-    Compute objective value for barycenter problem.
-
-    :param r: (n,) tensor (barycenter)
-    :param Xs: (m, n, n) tensor (transportation plans)
-    :param cost_mat: (n, n) tensor
-    :param kappas: list of 2 scalar tensors (kappa_r and kappa_c)
-    :param cs: (m, n) tensor (distributions, data points)
-    :return: scalar tensor
-    """
-    transport_cost = torch.tensordot(Xs, cost_mat.unsqueeze(0), dims=3)  # scalar tensor
-    penalty_r, penalty_c = marginals_residuals(r, Xs, cs)  # 2 scalar tensors
-    kappa_r, kappa_c = kappas
-    return transport_cost + kappa_r * penalty_r + kappa_c * penalty_c
-
-
-def map_to_simplex(theta, n_):
-    """
-    Map a vector of parameters from R^d to points on unit simplexes.
-
-    :param theta: (d,) tensor with d = n + m*n^2
-    :param n_: dimension of barycenter
-    :return: list of (n,) tensor (barycenter) and (m, n, n) tensor (plans)
-    """
-    z = theta[:n_]
-    Ys = theta[n_:].reshape(-1, n_ ** 2)
-    r = torch.softmax(z, dim=-1)
-    Xs = torch.softmax(Ys, dim=-1)
-    return r, Xs.reshape(-1, n_, n_)
-
-
-def get_data_and_solution(device, dtype=torch.float32, size=3):
+def get_data_and_solution(device, dtype=torch.float32, size=3, column_interval=1):
     """
     Get two simple 3x3 images and their barycenter.
 
     :param device: 'cuda' or 'cpu'
     :return: list of (2, 9) tensor (data points) and (9,) tensor (barycenter)
     """
+    barycenter = None
     cs = []
-    for i in range(size):
+    for i in range(0, size, column_interval):
         c = torch.zeros((size, size), device=device, dtype=dtype)
         c[:, i] = 1. / size
         c = c.flatten()
@@ -100,22 +53,6 @@ def get_data_and_solution(device, dtype=torch.float32, size=3):
 
     cs = torch.stack(cs)
     return cs, barycenter
-
-
-def get_optimal_plans(device):
-    """
-    Get optimal plans for simple 3x3 images.
-
-    :param device: 'cuda' or 'cpu'
-    :return: (2, 9, 9) tensor
-    """
-    im_sz = 3
-    X1 = torch.zeros(im_sz**2, im_sz**2, device=device)
-    X2 = torch.zeros_like(X1)
-    for i, idx_in_r in enumerate([1, 4, 7]):
-        X1[idx_in_r, i*3] = 1. / 3.
-        X2[idx_in_r, i*3 + 2] = 1. / 3.
-    return torch.stack([X1, X2])
 
 
 def get_init_barycenter(r_opt, noise_level, seed=None):
@@ -132,73 +69,15 @@ def get_init_barycenter(r_opt, noise_level, seed=None):
     return r_prior, z_prior
 
 
-def get_init_plans(r_prior, cs):
-    """Define initial transport plans that give correct marginals."""
-    X1 = r_prior.unsqueeze(1) @ cs[0].unsqueeze(0)
-    X2 = r_prior.unsqueeze(1) @ cs[1].unsqueeze(0)
-    Xs = torch.stack([X1, X2])
-
-    # map to the whole Euclidean space
-    Ys = safe_log(Xs)
-    return Xs, Ys
-
-
 def get_optimal_value(device, cost_mat, cs, r_opt):
     wasser_dist = [ot.emd2(c, r_opt, cost_mat) for c in cs]
     return sum(wasser_dist)
 
 
-def alg_full_sampling(kappa, std_decay, sample_size, n_steps, prior_std, noise_level, device):
-    cost_mat = get_cost_matrix(IMG_SIZE, device)
-    cs, r_opt = get_data_and_solution(device)
-    Xs_opt = get_optimal_plans(device)
-    kappas = [kappa, kappa]  # equal coefficients for penalizing both marginals
-
-    def objective(sample):
-        sample_size = sample.shape[0]
-        values = torch.empty(sample_size, device=device)
-        for i in range(sample_size):
-            r, Xs = map_to_simplex(sample[i], n)
-            values[i] = barycenter_objective(r, Xs, cost_mat, kappas, cs)
-        return -values  # minus because the algorithm maximizes the objective
-
-    # Define initial approx. barycenter and transport plans as well as their counterparts in R^n
-    r_prior, z_prior = get_init_barycenter(r_opt, noise_level, seed=0)
-    Xs, Ys = get_init_plans(r_prior, cs)
-    prior_mean = torch.cat((z_prior, Ys.flatten()))  # concatenate variables in one tensor
-
-    # the following function is needed to store info about convergence of the algorithm
-    def get_info(theta):
-        """
-        Get information about how far the current point from optimum.
-
-        :param theta: vector of variables at the current step
-        :return: list containing current approximation of barycenter and
-            information about how far the current point from optimum
-        """
-        r, Xs = map_to_simplex(theta, n)
-        residue_r, residue_c = marginals_residuals(r, Xs, cs)
-        acc_X1 = norm_sq(Xs[0] - Xs_opt[0])  # distance between current and optimal plans
-        acc_X2 = norm_sq(Xs[1] - Xs_opt[1])
-        acc_r = norm_sq(r - r_opt)  # distance between current approximation and true barycenter
-        objective_val = -objective(theta.unsqueeze(0)).item()
-        transport_cost = torch.tensordot(Xs, cost_mat.unsqueeze(0), dims=3).item()
-        return r, residue_r, residue_c, objective_val, transport_cost, acc_X1, acc_X2, acc_r
-
-    # Names and indices of elements of the list returned by 'get_info'
-    info_names = [{'Marginal error for r': 1, 'Marginal error for c': 2, 'Objective': 3, 'Transport cost': 4},
-                  {'Accuracy of X1': 5, 'Accuracy of X2': 6}, {'Accuracy of r': 7}]
-
-    trajectory = algorithm(prior_mean, prior_std, n_steps, sample_size, objective,
-                           std_decay=std_decay, seed=0, get_info=get_info, track_time=True)
-    n_cols = 6
-    img_name = f"kappa_{kappa}_sample_size_{sample_size}_prior_std_{prior_std}"
-    plot_trajectory(trajectory, n_cols, IMG_SIZE, img_name, info_names)
-
-
-def alg_ot_lib(std_decay, sample_size, n_steps, prior_std, noise_level, device, add_entropy=False, start_coef=20., decrease=0.25):
-    cost_mat = get_cost_matrix(IMG_SIZE, device, dtype=FLOAT64).numpy()
-    cs, r_opt = get_data_and_solution(device, dtype=FLOAT64)
+def experiment_pot(dtype, img_size, column_interval, n_steps, device, sample_size, prior_var, var_decay, noise_level, add_entropy=False,
+                   start_coef=20., decrease=0.25):
+    cost_mat = get_cost_matrix(img_size, device, dtype=dtype).numpy()
+    cs, r_opt = get_data_and_solution(device, dtype=dtype, size=img_size, column_interval=column_interval)
     cs = cs.numpy()
 
     if add_entropy:
@@ -222,16 +101,19 @@ def alg_ot_lib(std_decay, sample_size, n_steps, prior_std, noise_level, device, 
         else:  # evaluate objective for a sample of points
             single_sample = False
 
-        wasser_dist = [ot.emd2(c, barycenters, cost_mat) for c in cs]
+        wasser_dist = [ot.emd2(c, barycenters, cost_mat) for c in cs]  # ot. bregman.sinkhorn2 , reg=0.1
         entropy = reg_coef * scipy.stats.entropy(barycenters) if add_entropy and reg_coef else 0.  # shape (sample_size,)
 
         if single_sample:
             objective_val = sum(wasser_dist)
         else:  # wasser_dist is list of (sample_size,) tensors
-            objective_val = torch.tensor(wasser_dist, device=device, dtype=FLOAT64).sum(dim=0)
+            objective_val = torch.tensor(wasser_dist, device=device, dtype=dtype).sum(dim=0)
         return -objective_val + entropy  # minus because algorithm maximizes objective
 
-    r_prior, z_prior = get_init_barycenter(r_opt, noise_level, seed=0)  # initial approx. barycenter
+    r_prior = torch.ones_like(r_opt)
+    r_prior /= r_prior.sum()
+    z_prior = safe_log(r_prior)
+    # r_prior, z_prior = get_init_barycenter(r_opt, noise_level, seed=0)  # initial approx. barycenter
 
     # the following function is needed to store info about convergence of the algorithm
     def get_info(z):
@@ -250,7 +132,7 @@ def alg_ot_lib(std_decay, sample_size, n_steps, prior_std, noise_level, device, 
     # Names and indices of elements of the list returned by 'get_info'
     info_names = [{'Objective': 1}, {'Accuracy of r': 2}]
 
-    prior_cov = prior_std**2 * torch.eye(n, dtype=FLOAT64)
+    prior_cov = prior_var * torch.eye(img_size**2, dtype=dtype)
     def get_sample(mean, cov, seed):
         if seed is not None:
             torch.manual_seed(seed)
@@ -258,23 +140,64 @@ def alg_ot_lib(std_decay, sample_size, n_steps, prior_std, noise_level, device, 
         return distr.sample((sample_size,))
 
     def recalculate_cov(old_cov, sample, step, weights):
-        return std_decay * torch.cov(sample.T, aweights=weights)
+        # empir_cov = torch.cov(sample.T, aweights=weights)
+        return var_decay * old_cov  # empir_cov
 
     trajectory = algorithm(z_prior, prior_cov, get_sample, n_steps, objective,
-                           recalculate_cov, seed=0, get_info=get_info, track_time=True, hyperparam=reg_coefs)
+                           recalculate_cov, seed=0, get_info=get_info, track_time=False, hyperparam=reg_coefs)
     n_cols = 6
-    img_name = f"COV_samples_{sample_size}_std_{prior_std}_decay_{std_decay}"
-    opt_val = get_optimal_value(device, cost_mat, cs, r_opt.numpy())  # needed for displaying optimal value on plot
-    plot_trajectory(trajectory, n_cols, IMG_SIZE, img_name, info_names, opt_val=opt_val)
+    img_name = f"samples_{sample_size}_var_{prior_var}_decay_{var_decay}"
+    # opt_val = get_optimal_value(device, cost_mat, cs, r_opt.numpy())  # needed for displaying optimal value on plot
+    # plot_trajectory(trajectory, n_cols, img_size, img_name, info_names, opt_val=opt_val)
+    return sum([info[2] for info in trajectory[-3:]]) / 3, trajectory
+
+
+def main():
+    dtype = torch.float64  # avoid errors when converting from torch to numpy
+    img_size = 5  # image size is DxD
+    column_interval = 1
+    n_steps = 50  # number of iterations
+
+    n_cols = 6
+    info_names = [{'Objective': 1}, {'Accuracy of r': 2}]
+
+    # Hyperparameters
+    device = 'cpu'
+    sample_sizes = [8 ** 4]  # [8 ** i for i in range(3, 6)]
+    # sample_size = 1000
+    prior_vars = [2. ** i for i in range(3, 5)]
+    # prior_var = 7.  # initial variance of prior
+    var_decays = [0.65, 0.8, 0.95]
+    # var_decay = 0.85  # decay rate for variance
+    noise_level = 0.4  # initial mean of prior is defined as log(true barycenter + noise)
+
+    for sample_size in sample_sizes:
+        print("SAMPLE SIZE:", sample_size)
+        best_acc = 10. ** 6
+        best_hyperpar = None
+        best_traj = None
+        for prior_var in prior_vars:
+            for var_decay in var_decays:
+                start = time.time()
+                acc_r, traj = experiment_pot(dtype, img_size, column_interval, n_steps, device, sample_size, prior_var, var_decay, noise_level)
+                print(f"Experiment took {time.time() - start:.2f} s")
+                if acc_r < best_acc:
+                    best_acc = acc_r
+                    best_hyperpar = (prior_var, var_decay)
+                    best_traj = traj
+                else:
+                    del traj
+                gc.collect()
+        print(f"Best prior_var: {best_hyperpar[0]}, best var_decay: {best_hyperpar[1]}")
+        img_name = f"5_samples_{sample_size}_var_{best_hyperpar[0]}_decay_{best_hyperpar[1]}"
+        plot_trajectory(best_traj, n_cols, img_size, img_name, info_names)
+
+    # prior_stds = [2.5]  # test various standard deviations for sampling
+    # std_decays = [0.9]  # test various decay rates of standard deviation for sampling
+    # for std_decay in std_decays:
+    #     for prior_std in prior_stds:
+    #          experiment_pot(std_decay, sample_size, n_steps, float(prior_std), noise_level, device, add_entropy=False)
 
 
 if __name__ == "__main__":
-    device = 'cpu'
-    noise_level = 0.4  # initial point is defined as the true barycenter + noise
-    prior_stds = [2.5]  # test various standard deviations for sampling
-    std_decays = [0.85]  # test various decay rates of standard deviation for sampling
-    n_steps = 20  # number of iterations
-    sample_size = 10000
-    for std_decay in std_decays:
-        for prior_std in prior_stds:
-            alg_ot_lib(std_decay, sample_size, n_steps, float(prior_std), noise_level, device, add_entropy=False)
+    main()
--- utils.py
+++ utils.py
@@ -17,12 +17,16 @@ def norm_sq(tensor):
     return (tensor ** 2).sum().item()
 
 
-def show_barycenters(barycenters, img_sz, img_name):
+def show_barycenters(barycenters, img_sz, img_name, iterations=None):
     """Display several barycenters across iterations."""
     fig, ax = plt.subplots(nrows=1, ncols=len(barycenters), figsize=(16, 4))
     for i, z in enumerate(barycenters):
         img = torch.softmax(z, dim=-1).cpu().numpy().reshape(img_sz, -1)
         ax[i].imshow(img, cmap='binary')
+
+    if iterations is not None:
+        for i, iter in enumerate(iterations):
+            ax[i].title.set_text(f"Iteration {iter}")
 
     plt.savefig(f"plots/bary_{img_name}.png", bbox_inches='tight')
     plt.close()
@@ -38,7 +42,8 @@ def plot_convergence(trajectory, img_name, info_names, log_scale=False, opt_val=
         for name, idx in names.items():
             ax.plot([el[idx] for el in trajectory[1:]], label=name)
             if name == 'Objective' and opt_val is not None:
-                ax.hlines(y=opt_val, xmin=0, xmax=len(trajectory)-1, color='green', label='Optimal value')
+                ax.hlines(y=opt_val, xmin=0, xmax=len(trajectory)-2, color='green', label='Optimal value')
+        ax.yaxis.grid()
 
         ax.legend()
         if log_scale:
@@ -53,8 +58,67 @@ def plot_trajectory(trajectory, n_cols, img_sz, img_name, info_names, log_scale=
     with open(f'trajectory_{img_name}.pickle', 'wb') as handle:
         pickle.dump(trajectory, handle)
 
-    n_steps = len(trajectory)
-    plot_every = int(n_steps / n_cols) + 1 if n_steps % n_cols else int(n_steps / n_cols)
-    barycenters = [el[0] for el in trajectory[::plot_every]]
-    show_barycenters(barycenters, img_sz, img_name)
+    n_steps = len(trajectory) - 1
+    # plot_every = int(n_steps / n_cols) + 1 if n_steps % n_cols else int(n_steps / n_cols)
+    slope = n_steps / (n_cols - 1)
+    iterations = [int(i * slope) for i in range(n_cols)]
+    barycenters = [trajectory[it][0] for it in iterations]  # [el[0] for el in trajectory[::plot_every]]
+    show_barycenters(barycenters, img_sz, img_name, iterations=iterations)
     plot_convergence(trajectory, img_name, info_names, log_scale=log_scale, opt_val=opt_val)
+
+
+def compare_trajectories(file_names, plot_names, info_names, n_cols, img_sz):
+    trajs = []
+    for name in file_names:
+        with open(f'{name}.pickle', 'rb') as handle:
+            trajs.append(pickle.load(handle))
+
+    n_steps = len(trajs[0]) - 1
+    slope = n_steps / (n_cols - 1)
+    iterations = [int(i * slope) for i in range(n_cols)]
+
+    # fig, axs = plt.subplots(nrows=len(file_names), ncols=n_cols, figsize=(16, 14))
+    fig, axs = plt.subplots(nrows=len(file_names), ncols=1, constrained_layout=True)
+
+    for ax in axs:
+        ax.remove()
+    gridspec = axs[0].get_subplotspec().get_gridspec()
+    subfigs = [fig.add_subfigure(gs) for gs in gridspec]
+
+    for row, subfig in enumerate(subfigs):
+        barycenters = [trajs[row][it][0] for it in iterations]
+        subfig.suptitle(r'$\bf{' + plot_names[row].replace(" ", "\ ") + "}$")
+        axs = subfig.subplots(nrows=1, ncols=n_cols)
+        for j, z in enumerate(barycenters):
+            # for col, ax in enumerate(axs):
+            img = torch.softmax(z, dim=-1).cpu().numpy().reshape(img_sz, -1)
+            axs[j].imshow(img, cmap='binary')
+            axs[j].set_title(f"step {iterations[j]}")
+            axs[j].axes.xaxis.set_visible(False)
+            axs[j].axes.yaxis.set_visible(False)
+
+    plt.savefig(f"plots/5_comparison_bary.png")
+    plt.close()
+
+    n_plots = len(info_names)
+    figsize = (7, 4 * n_plots)
+    fig, axs = plt.subplots(n_plots, 1, figsize=figsize)
+    for i, names in enumerate(info_names):
+        ax = axs[i]
+        for name, idx in names.items():
+            for trajectory, plot_name in zip(trajs, plot_names):
+                ax.plot([el[idx] for el in trajectory[1:]], label=plot_name)
+            ax.set_title(name)
+        ax.legend()
+        ax.yaxis.grid()
+
+    plt.tight_layout()
+    plt.savefig(f"plots/5_comparison_convergence.png", bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    file_names = [f'trajectory_5_samples_512_var_16.0_decay_0.8']\
+               + [f'trajectory_5_samples_{8 ** i}_var_4.0_decay_1.0' for i in range(4, 6)]
+    plot_names = [f'{8 ** i} samples' for i in range(3, 6)]
+    info_names = [{'Objective': 1}, {'Squared error': 2}]
+    compare_trajectories(file_names, plot_names, info_names, 6, 5)
