563c0bb29ee8c14655bb9ef1b3f88839b737fac4
Egor Gladin <gladin.el@phystech.edu>
2022-04-12 18:18:48 +0200

    Added tests with digits

--- potentials_tests.py
+++ potentials_tests.py
@@ -5,20 +5,29 @@ from experiment_barycenter import get_cost_matrix, get_data_and_solution
 from utils import replace_zeros, show_barycenters
 from kantorovich_dual import get_c_concave
 
+from tqdm.contrib.itertools import product
 
-def visualize_potentials(img_sz, cs, r_opt, cost_mat, kappa, image_name, reverse_order=False):
-    # Direct order
-    u = []
-    v = []
-    for c in cs:
-        res = ot.emd(c, r_opt, cost_mat, log=True)[1] if reverse_order\
-            else ot.emd(r_opt, c, cost_mat, log=True)[1]
-        u.append(res['u'])
-        v.append(res['v'])
 
-    u = torch.cat(u).unsqueeze(0)  # (1, m*n)
+def visualize_potentials(img_sz, cs, r_opt, cost_mat, kappa, image_name, use_emd=True,
+                         calc_poten=True, poten_path=None, sinkhorn_reg=0.01):
+    if calc_poten:
+        u = []
+        v = []
+        for c in cs:
+            res = ot.emd(r_opt, c, cost_mat, log=True)[1] if use_emd\
+                else ot.sinkhorn(r_opt, c, cost_mat, sinkhorn_reg, log=True, stopThr=1e-10, numItermax=50000)[1]
+            u.append(res['u'])
+            v.append(res['v'])
+
+        u, v = torch.cat(u), torch.cat(v)
+        torch.save(u, poten_path + '_u.pt')
+        torch.save(v, poten_path + '_v.pt')
+    else:
+        u = torch.load(poten_path + '_u.pt', map_location=cs.device)
+        v = torch.load(poten_path + '_v.pt', map_location=cs.device)
+
+    u, v = u.unsqueeze(0), v.unsqueeze(0)
     u_c, u = get_c_concave(u, cost_mat, double=True)  # (1, m, n)
-    v = torch.cat(v).unsqueeze(0)
     v_c, v = get_c_concave(v, cost_mat, double=True)  # (1, m, n)
 
     u_sum = u.squeeze(0).sum(dim=0)  # (n,)
@@ -53,27 +62,19 @@ def digits_problem():
     cs = torch.load(folder + 'digits.pt', map_location=device)
     r = torch.load(folder + 'barycenter.pt', map_location=device)
 
-    images = [c for c in cs] + [r]
-    titles = [f'c{i}' for i in range(cs.shape[0])] + ['ot.barycenter']
-    show_barycenters(images, img_sz, 'digit_inputs', use_softmax=False, iterations=titles, scaling='partial')
+    # images = [c for c in cs] + [r]
+    # titles = [f'c{i}' for i in range(cs.shape[0])] + ['ot.barycenter']
+    # show_barycenters(images, img_sz, 'digit_inputs', use_softmax=False, iterations=titles, scaling='partial')
 
-    for kappa in [1., 0.1, 0.01]:
-        image_name = f'digits_direct_kappa_{kappa}'
-        visualize_potentials(img_sz, cs, r, cost_mat, kappa, image_name)
+    kappas = [0.1, 0.3, 0.6]
+    regs = [0.1, 0.5, 1., 2.]
 
-
-def toy_problem():
-    device = 'cpu'
-    img_sz = 3
-    dtype = torch.float64
-    cost_mat = get_cost_matrix(img_sz, device, dtype=dtype)
-    cs, r_opt = get_data_and_solution(device, dtype=dtype, size=img_sz)
-    for kappa in [1., 0.01]:
-        image_name = f'3x3_direct_kappa_{kappa}'
-        visualize_potentials(img_sz, cs, r_opt, cost_mat, kappa, image_name)
+    for kappa, reg in product(kappas, regs):
+        calc_poten = (kappa == kappas[0])
+        image_name = f'digits_sinkh_kappa_{kappa}_reg_{reg}'
+        visualize_potentials(img_sz, cs, r, cost_mat, kappa, image_name, use_emd=False,
+                             calc_poten=calc_poten, poten_path=folder+f'sinhk_reg{reg}', sinkhorn_reg=reg)
 
 
 if __name__ == "__main__":
-    # kappa = 0.01
     digits_problem()
-    # toy_problem()
