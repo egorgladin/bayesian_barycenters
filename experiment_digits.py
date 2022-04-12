Corrected weighting process

experiment_digits.py
@@ -138,7 +138,7 @@ def get_poten_from_alg(cs, cost_mat, device):
 
 def baseline(wass_params, kappa, device='cuda', calc_poten_method='sinkhorn',
              calc_poten=True, reverse_order=False, do_sampling=False, sample_size=1000000,
-             n_batches=100, calc_weights=True, temperature=10., calc_posterior=True):
+              prior_var=0.001, n_batches=100, calc_weights=True, temperature=10., calc_posterior=True):
     assert calc_poten_method in ['emd', 'vaios', 'alg', 'sinkhorn']
     assert sample_size % n_batches == 0
     dtype = torch.float64
@@ -241,7 +241,7 @@ def baseline(wass_params, kappa, device='cuda', calc_poten_method='sinkhorn',
     # 5. Sample potentials and calculate empirical mean and covariance
     if do_sampling:
         get_sample = get_sampler(sample_size)
-        cov = 0.001 * torch.eye(n_data_points * n, dtype=dtype, device=device)
+        cov = prior_var * torch.eye(n_data_points * n, dtype=dtype, device=device)
         sample = get_sample(potentials.flatten(), cov, 0)
 
         increment = sample_size // n_batches
@@ -256,20 +256,19 @@ def baseline(wass_params, kappa, device='cuda', calc_poten_method='sinkhorn',
             torch.cuda.empty_cache()
 
     if calc_weights:
-        weight_list = []
+        objective_list = []
         print('Started calculating objective for batches')
         for i in tqdm(range(n_batches)):
             sample_batch = torch.load(folder + f'sample_batch{i}.pt', map_location=device)
             objective_values = objective_function(sample_batch, cost_mat, cs, kappa, double_conjugate=False)[0]
             del sample_batch
-            weights = torch.softmax(temperature * objective_values, dim=-1)
+            objective_list.append(objective_values.clone())
             del objective_values
-            weight_list.append(weights.clone())
-            del weights
             if device == 'cuda':
                 torch.cuda.empty_cache()
 
-        all_weights = torch.cat(weight_list)
+        all_objective_vals = torch.cat(objective_list)
+        all_weights = torch.softmax(temperature * all_objective_vals, dim=-1)
         torch.save(all_weights, folder + 'all_weights.pt')
     else:
         all_weights = torch.load(folder + 'all_weights.pt', map_location=device)
@@ -322,4 +321,4 @@ if __name__ == "__main__":
     calc_poten = False  # don't calculate potentials again, just load them from memory
     do_sampling = True  # don't draw a large number of samples, just load empirical mean and cov from memory
     baseline(None, kappa, device='cpu', calc_poten_method=calc_poten_method, calc_poten=calc_poten, reverse_order=False,
-             do_sampling=do_sampling, sample_size=1000000)
+             do_sampling=do_sampling, sample_size=1000000, prior_var=1e-4, temperature=20.)